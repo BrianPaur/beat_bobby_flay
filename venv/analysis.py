4a8b2560720ec9dce41c438a78fae24e31b5a93d
Brian Paur <brian.paur@gmail.com>
2022-06-21 20:37:37 -0500

    Updated data storage to db using sqlite3. Also, formatted data so that it's a single table and added season to last column. Data should be easier to filter through and pulls should be much faster

--- venv/analysis.py
+++ venv/analysis.py
@@ -2,227 +2,315 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
-from openpyxl import Workbook, load_workbook
-import openpyxl
-import os
-from os.path import exists
+import sqlite3
+from sqlalchemy import create_engine
 import gender_guesser.detector as gender
+import os
 
-#######################################################################
-#### Main data pull ###################################################
-#######################################################################
 
 class Datapull:
+    def __init__(self, db_bobby=None):
+        self.db_bobby = db_bobby
 
-    def __init__(self, wbname="Beat_Bobby_Flay.xlsx", wbseason="Beat_Bobby_Flay_season_data.xlsx"):
-        self.wbname = wbname
-        self.wbseason = wbseason
+    #######################################################################
+    #### Main data pull ###################################################
+    #######################################################################
 
-    def data_pull(self):
+    def db_table(self):
+        # establishes db connection
+        connection = self.db_bobby
+        cursor = connection.cursor()
 
-        # creates workbook
-        w = Workbook()
-        w.save(self.wbname)
-        w.close()
+        # tries to create table is none exists
+        try:
+            cursor.execute(
+                "CREATE TABLE beat_bobby_flay (episode_no INTEGER UNIQUE, season_episode_no INTEGER, title TEXT, original_air_date TEXT, guests TEXT, ingredients TEXT, contestants TEXT, judges TEXT, dish TEXT, winner TEXT, season INTEGER)"
+            )
+        except:
+            pass
 
+        df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+
+        return df
+
+    def number_of_seasons(self):
         # url for data
         wikiurl = "https://en.wikipedia.org/wiki/Beat_Bobby_Flay"
         response = requests.get(wikiurl)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
+        dom = etree.HTML(str(soup))
+        return int(
+            dom.xpath('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[8]/td')[
+                0
+            ].text
+        )
 
-        # get the number of seasons from wiki table
-        number_of_seasons = int(
-            etree.fromstring(response.text).xpath('/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[8]/td')[
-                0].text)
+    def season_data(self):
 
-        # finds the different season tables on the wiki page
-        my_tables = soup.find_all("table", {"class": "wikitable"})
-        df = pd.read_html(str(my_tables))
-
-        # creates season data workbook
-        writer = pd.ExcelWriter(self.wbseason, engine='openpyxl')
-
-        # names sheets
-        for i in range(0, number_of_seasons):
-            pd.DataFrame(df[i]).to_excel(writer, f"season {i + 1}")
-            writer.save()
-
-        writer.close()
-
-        # formats the sheets to remove unnecessary headers
-        for i in range(0, number_of_seasons):
-            wb = load_workbook(self.wbseason)
-            ws = wb[f'season {i + 1}']
-            ws.delete_rows(3, 1)
-            wb.save(self.wbseason)
-
-        wb.close()
-
-#######################################################################
-#### Formats the workbooks ############################################
-#######################################################################
-
-class Format:
-
-    def __init__(self, wbname="Beat_Bobby_Flay.xlsx", wbseason="Beat_Bobby_Flay_season_data.xlsx"):
-        self.wbname = wbname
-        self.wbseason = wbseason
-
-    def format(self):
-        all_df = pd.read_excel(self.wbseason, sheet_name=None)
-        df = pd.concat(all_df, ignore_index=True)
-        df = df.rename(columns=df.iloc[0]).drop(df.index[0])  # drops headers that were taking up space
-        df = df[df['Title'] != 'Title']
-
-        # creates wb for all data to be combine on
-        writer = pd.ExcelWriter(self.wbname, engine='openpyxl')
-        df.to_excel(writer, "All Data")
-        writer.save()
-        writer.close()
-
-#######################################################################
-#### Analysis functions ###############################################
-#######################################################################
-
-class Analysis:
-
-    def __init__(self, df=pd.read_excel("Beat_Bobby_Flay.xlsx"), df_by_season="Beat_Bobby_Flay_season_data.xlsx"):
-        self.df = df
-        self.df_by_season = df_by_season
-
-    # returns whole dataframe
-    def data_frame(self):
-        print(self.df)
-
-    # calculates total amount of challeges
-    def total_challanges(self):
-        print(len(self.df.index))
-
-    # calculates win breakdown
-    def win_breakdown(self):
-        data = self.df['Winner'].value_counts()
-
-        print(f'total challeges bobby has won: {data.head(1)[0]} out of {len(data)+data.head(1)[0]}')
-
-    # calculates win breakdown by season
-    def win_breakdown_by_season(self, season=None):
-        #defaults to the latest season if none provided
-        if season == None:
+        try:
             # url for data
             wikiurl = "https://en.wikipedia.org/wiki/Beat_Bobby_Flay"
             response = requests.get(wikiurl)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            season = int(etree.fromstring(response.text).xpath('/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[8]/td')[0].text)
+            soup = BeautifulSoup(response.content, "html.parser")
+            my_tables = soup.find_all("table", {"class": "wikitable"})
+            df = pd.read_html(str(my_tables))
+            seasons = self.number_of_seasons()
 
-            df = pd.read_excel(self.df_by_season,sheet_name=f'season {season}')
-            df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+            for i in range(1, seasons + 1):
+                seasons_list_range = i - 1
 
-            print(f'total challeges {len(df.index)}')
-            print(df['Winner'].value_counts())
+                # selects season from table list
+                df1 = df[(i - 1)]
 
+                # adds season column
+                df1["season"] = None
+
+                # add season number to dataframe
+                df1 = df1.assign(season=i)
+
+                # reassigns the headers
+                df1.columns = [
+                    "episode_no",
+                    "season_episode_no",
+                    "title",
+                    "original_air_date",
+                    "guests",
+                    "ingredients",
+                    "contestants",
+                    "judges",
+                    "dish",
+                    "winner",
+                    "season",
+                ]
+
+                print(df1)
+
+                engine = create_engine("sqlite:///BBF.db", echo=False)
+
+                df1.to_sql(
+                    "beat_bobby_flay", con=engine, if_exists="append", index=False
+                )
+        except:
+            print("all data up to date")
+
+    #######################################################################
+    #### Analysis functions ###############################################
+    #######################################################################
+
+    # returns whole dataframe
+    def read_table(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
+
+        if season == None:
+            print(pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection))
+        elif season > 0 and season <= self.number_of_seasons():
+            print(
+                pd.read_sql_query(
+                    "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                    connection,
+                    params=(season,),
+                )
+            )
         else:
-            df = pd.read_excel(self.df_by_season,sheet_name=f'season {season}')
-            df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+            print(f"{season} doesn't exist or hasn't aired yet")
 
-            print(f'total challeges {len(df.index)}')
-            print(df['Winner'].value_counts())
+    # calculates total amount of challeges
+    def total_challanges(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
+
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            return len(df.index)
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            return len(df.index)
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
+
+    # calculates win breakdown
+    def win_breakdown(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
+
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            data = df["winner"].value_counts()
+            print(
+                f"total challeges bobby has won: {data.head(1)[0]} out of {len(data) + data.head(1)[0]}"
+            )
+
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            data = df["winner"].value_counts()
+            print(
+                f"total challeges bobby has won: {data.head(1)[0]} out of {len(data) + data.head(1)[0]}"
+            )
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
 
     # calculates bobby's w/l percentage
-    def bobby_win_rate(self):
-        wins = self.df['Winner'].value_counts().head(1)
-        print(f'Bobby has a {round(wins[0]/len(self.df.index)*100,2)}% win rate')
+    def bobby_win_rate(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
 
-    # calculates bobby's w/l percentage by season
-    def bobby_win_rate_by_season(self):
-        # url for data for number of seasons
-        wikiurl = "https://en.wikipedia.org/wiki/Beat_Bobby_Flay"
-        response = requests.get(wikiurl)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        season = int(etree.fromstring(response.text).xpath('/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[8]/td')[0].text)
-
-        rows = []
-
-        for i in range(1,(season+1)):
-
-            df = pd.read_excel(self.df_by_season, sheet_name=f'season {i}')
-            df = df.rename(columns=df.iloc[0]).drop(df.index[0])
-
-            wins = df['Winner'].value_counts().head(1)
-            len_of_season = len(df.index)
-
-            rows.append([i, round(wins[0] / len(df.index) * 100, 2),len_of_season])
-
-        # sets up new dataframe
-        df2 = pd.DataFrame(rows, columns=["Season", "Win Rate", "Number of Episodes"])
-
-        print(df2.to_string(index=False))
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            wins = df["winner"].value_counts().head(1)
+            print(f"Bobby has a {round(wins[0] / len(df.index) * 100, 2)}% win rate")
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            wins = df["winner"].value_counts().head(1)
+            print(
+                f"Bobby has a {round(wins[0] / len(df.index) * 100, 2)}% win rate for season {season}"
+            )
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
 
     # shows winners dish
-    def winners_dish(self):
-        winners_dish = df[self.df['Winner'] != 'Bobby Flay']
-        winners_dish = winners_dish[['Dish','Winner']]
-        print(winners_dish)
+    def winners_dish(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
 
-    # shows winners dish by season
-    def winners_dish_by_season(self, season=None):
         if season == None:
-            # url for data for number of seasons
-            wikiurl = "https://en.wikipedia.org/wiki/Beat_Bobby_Flay"
-            response = requests.get(wikiurl)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            season = int(etree.fromstring(response.text).xpath('/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[8]/td')[0].text)
-
-            df = pd.read_excel(self.df_by_season, sheet_name=f'season {season}')
-            df = df.rename(columns=df.iloc[0]).drop(df.index[0])
-            if df[df['Winner'] != 'Bobby Flay'].count().all() == 0:
-                print('Bobby won every episode...')
-            else:
-                winners_dish = df[df['Winner'] != 'Bobby Flay']
-                winners_dish = winners_dish[['Dish','Winner']]
-                print(winners_dish)
-
-        else:
-            df = pd.read_excel(self.df_by_season, sheet_name=f'season {season}')
-            df = df.rename(columns=df.iloc[0]).drop(df.index[0])
-            winners_dish = df[df['Winner'] != 'Bobby Flay']
-            winners_dish = winners_dish[['Dish','Winner']]
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            winners_dish = df[df["winner"] != "Bobby Flay"]
+            winners_dish = winners_dish[["winner", "dish"]]
             print(winners_dish)
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            winners_dish = df[df["winner"] != "Bobby Flay"]
+            winners_dish = winners_dish[["winner", "dish"]]
+            print(winners_dish)
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
 
     # shows date of losses
-    def loss_date(self):
-        df = self.df
-        df = df[self.df['Winner'] != 'Bobby Flay']
-        df = df['Original airdate']
-        print(df)
+    def loss_date(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
+
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            df = df[df["winner"] != "Bobby Flay"]
+            df = df[["winner", "original_air_date"]]
+            print(df)
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            df = df[df["winner"] != "Bobby Flay"]
+            df = df[["winner", "original_air_date"]]
+            print(df)
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
 
     # tallies up the losses by year
-    def loss_year(self):
-        df = self.df
-        df = df[self.df['Winner'] != 'Bobby Flay']
-        df = df['Original airdate'].str[-4:].value_counts().sort_index(ascending=False)
-        print(df)
+    def loss_by_year(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
+
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            df = df[df["winner"] != "Bobby Flay"]
+            df = (
+                df["original_air_date"]
+                .str[-4:]
+                .value_counts()
+                .sort_index(ascending=False)
+            )
+            print(df)
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            df = df[df["winner"] != "Bobby Flay"]
+            df = (
+                df["original_air_date"]
+                .str[-4:]
+                .value_counts()
+                .sort_index(ascending=False)
+            )
+            print(df)
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
 
     # tallies up the losses by month
-    def loss_month(self):
-        cat = ['Jan', 'Feb', 'Mar', 'Apr','May','Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec']
-        df = self.df
-        df = df[self.df['Winner'] != 'Bobby Flay']
-        df = df['Original airdate'].str[:3].value_counts().reindex(cat)
-        print(df)
+    def loss_month(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
+
+        cat = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+        ]
+
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            df = df[df["winner"] != "Bobby Flay"]
+            df = df["original_air_date"].str[:3].value_counts().reindex(cat)
+            print(df)
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            df = df[df["winner"] != "Bobby Flay"]
+            df = df["original_air_date"].str[:3].value_counts().reindex(cat)
+            print(df)
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
 
     # tallies up the losses by day of month
-    def time_of_month(self):
-        df = self.df
-        df = df[self.df['Winner'] != 'Bobby Flay']
-        df = df['Original airdate'].str[-8:-6].value_counts()
-        print(df)
+    def day_of_month(self, season=None):
+        connection = self.db_bobby
+        cursor = connection.cursor()
 
-
-
-
-
-
-
-
-
-
+        if season == None:
+            df = pd.read_sql_query("SELECT * FROM beat_bobby_flay", connection)
+            df = df[df["winner"] != "Bobby Flay"]
+            df = df["original_air_date"].str[-8:-6].value_counts()
+            print(df)
+        elif season > 0 and season <= self.number_of_seasons():
+            df = pd.read_sql_query(
+                "SELECT * FROM beat_bobby_flay WHERE season = ?",
+                connection,
+                params=(season,),
+            )
+            df = df[df["winner"] != "Bobby Flay"]
+            df = df["original_air_date"].str[-8:-6].value_counts()
+            print(df)
+        else:
+            print(f"{season} doesn't exist or hasn't aired yet")
--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,11 @@
-from analysis import Datapull, Format, Analysis
+from analysis import Datapull
 import pandas as pd
+import sqlite3
+
+pd.options.display.width = 0
+pd.options.display.max_rows = 0
 
 # example of how to run
 
 if __name__ == "__main__":
-    Datapull().data_pull()
-
-    Format().format()
-
-    Analysis().bobby_win_rate_by_season()
-
+    Datapull(sqlite3.connect("BBF.db")).read_table()
